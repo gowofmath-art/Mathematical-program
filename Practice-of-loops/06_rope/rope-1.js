e9f3457b40d9e1b00b4d9dea5414a6fc0a0cb310
gowofmath-art <gowofmath@gmail.com>
2026-08-16 12:35:46 +0800

    更新06

--- Practice-of-loops/06_rope/rope-1.js
+++ Practice-of-loops/06_rope/rope-1.js
@@ -1,4 +1,16 @@
 // 6.	迴圏的練習-rope
 // 若有一條繩子長3000公尺，每天剪去一半的長度，需多少天繩子的長度會短於5公尺。
+// 法一：用while 迴圈
 
+function rope() {
+    let n = 3000;
+    let count = 0;
+    while (n > 5) {
+        n = n/2;
+        count = count + 1
+    }
+    return count
+}
+
+console.log(rope())
 
